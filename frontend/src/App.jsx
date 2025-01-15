5a6ca471369ff854f284a8a66ac5903260062152
rahulmareja1710 <122693043+rahulmareja1710@users.noreply.github.com>
2025-01-15 17:10:22 +0530

    src created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WebAgent from './components/WebAgent'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6">
+      <WebAgent />
+    </div>
+  )
+}
+
+export default App
